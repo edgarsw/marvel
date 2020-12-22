58460e0efedc7aeee93d0ee21f143f3d1dbd943f
edgarsw <64525335+edgarsw@users.noreply.github.com>
2020-12-22 12:12:06 -0600

    Add files via upload

--- src/main/java/test/albo/mx/marvel/dao/ComicDao.java
+++ src/main/java/test/albo/mx/marvel/dao/ComicDao.java
@@ -11,14 +11,25 @@ import org.springframework.data.repository.CrudRepository;
 import test.albo.mx.marvel.model.Comic;
 
 /**
+ * Interface que contiene los metodos crud inyectados por spring.
  *
- * @author edgar
+ * @version 1.0, 21/12/20
+ * @author Edgar Salinas
  */
 public interface ComicDao extends CrudRepository<Comic, Long> {
 
-    @Query(value = "SELECT * from comic"
-            + " where idcomicservice in(select idcomicservice from comic where idcharacter = 1009149)"
-            + " and idcharacter <> 1009149",
+    /** 
+     * Consulta nativa a la base de datos.
+     * Proyecta los comics de un determinado personaje
+     * @param idPerson De tipo Integer es el identificador del personaje
+     * @return retorna una lista de objetos tipo Comic, registros
+     * encontrados en la base de datos
+     */
+    @Query(value = "SELECT idcomic, idcomicservice, title, name, person.idcharacter "
+            + " from comic inner join person using(idcharacter) "
+            + " where idcomicservice in"
+            + "(select idcomicservice from comic where idcharacter = ?1)"
+            + " and idcharacter <> ?1",
             nativeQuery = true)
     public List<Comic> findCharacters(Integer idPerson);
 }
